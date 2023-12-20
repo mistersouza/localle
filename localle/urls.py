d6f525b7af9a1314eea1382b4cb2a88bffc6cddb
Thiago Souza <mistersouza@icloud.com>
2023-12-20 18:15:53 +0000

    Add authentication

--- localle/urls.py
+++ localle/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("", include("core.urls"), name="core-urls"),
     path('admin/', admin.site.urls),
 ]
