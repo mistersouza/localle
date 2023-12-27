269d83a573ad4d766af8a50d2b34df560e6f0ebf
Thiago Souza <mistersouza@icloud.com>
2023-12-27 13:50:42 +0000

    Reafactor and comment function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,30 @@
-// Remove alert messages after 5 seconds
-setTimeout(function() {
+/**
+ * Remove alert messages after a delay of 5 seconds.
+ * Automatically removes alert elements with the '.alert' class from the DOM.
+ */
+setTimeout(() => {
     let messages = document.querySelectorAll('.alert');
-    messages.forEach(function(message) {
+    messages.forEach(message => {
         message.remove();
     });
-}, 5000); 
-
-// Reset search 
+}, 5000);
+/**
+ * Reset search functionality for the search form.
+ * Triggers search submission upon input changes and search button clicks.
+ * Submits the form when the input field is cleared.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.querySelector('#searchForm');
     const searchInput = document.querySelector('input[name="search"]');
+    const searchButton = document.querySelector('#searchButton');
+
+    searchButton.addEventListener('click', () => {
+        searchForm.submit();
+    });
 
     searchInput.addEventListener('input', () => {
-        searchForm.submit();
+        if (searchInput.value === '') {
+            searchForm.submit();
+        }
     });
 });
