f8a909cd4ba0b245c529e4474bd81e25c226f575
Thiago Souza <mistersouza@icloud.com>
2023-12-28 16:21:13 +0000

    Testing, js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,88 +3,98 @@
  * Automatically removes alert elements with the '.alert' class from the DOM.
  */
 setTimeout(() => {
-    let messages = document.querySelectorAll('.alert');
-    messages.forEach(message => {
-        message.remove();
+    const messages = document.querySelectorAll('.alert');
+    messages.forEach((message) => {
+      message.remove();
     });
-}, 5000);
-/**
- * Reset search functionality for the search form.
- * Triggers search submission upon input changes and search button clicks.
- * Submits the form when the input field is cleared.
- */
-document.addEventListener('DOMContentLoaded', () => {
+  }, 5000);
+  
+  /**
+   * Reset search functionality for the search form.
+   * Triggers search submission upon input changes and search button clicks.
+   * Submits the form when the input field is cleared.
+   */
+  document.addEventListener('DOMContentLoaded', () => {
     const searchForm = document.querySelector('#searchForm');
     const searchInput = document.querySelector('input[name="search"]');
     const searchButton = document.querySelector('#searchButton');
-    
+  
     const debounce = (func, delay) => {
-        let timeoutId;
-        return (...args) => {
-            if (timeoutId) {
-                clearTimeout(timeoutId);
-            }
-            timeoutId = setTimeout(() => {
-                func.apply(null, args);
-            }, delay);
-        };
-    };
-
-    const delayedSubmit = debounce(() => {
-        if (searchInput.value === '') {
-            searchForm.submit();
+      let timeoutId;
+      return (...args) => {
+        if (timeoutId) {
+          clearTimeout(timeoutId);
         }
+        timeoutId = setTimeout(() => {
+          func(null, ...args);
+        }, delay);
+      };
+    };
+  
+    const delayedSubmit = debounce(() => {
+      if (searchInput.value === '') {
+        searchForm.submit();
+      }
     }, 300);
-
+  
     if (searchButton) {
-        searchButton.addEventListener('click', () => {
-            searchForm.submit();
-        });
-
-        searchInput.addEventListener('input', delayedSubmit);
+      searchButton.addEventListener('click', () => {
+        searchForm.submit();
+      });
+  
+      searchInput.addEventListener('input', delayedSubmit);
     }
-});
-
-
-let deleteConfirmed = false;
-
-function toggleDeleteConfirmation() {
+  });
+  
+  /**
+   * Triggers a click event on the delete link.
+   */
+  function deleteItem() {
+    document.getElementById('deleteLink').click();
+  }
+  
+  let deleteConfirmed = false;
+  
+  /**
+   * Resets the UI buttons to their default state after delete confirmation.
+   */
+  function resetButtons() {
     const cancelButton = document.querySelector('#cancelButton');
     const deleteButton = document.getElementById('deleteButton');
-    const editButton = document.querySelector('#editButton'); 
-
-    if (!deleteConfirmed) {
-        deleteButton.innerHTML = `
-            <i class="fa-solid fa-exclamation-circle me-1"></i>
-            Confirm Delete
-        `;
-        cancelButton.classList.remove('d-none'); 
-        deleteButton.classList.remove('btn-outline-danger');
-        deleteButton.classList.add('btn-danger');
-        editButton.classList.add('d-none'); 
-        deleteConfirmed = true;
-    } else {
-        deleteItem()
-        resetButtons();
-    }
-}
-
-function resetButtons() {
-    const cancelButton = document.querySelector('#cancelButton');
-    const deleteButton = document.getElementById('deleteButton');
-    const editButton = document.querySelector('#editButton'); 
-
+    const editButton = document.querySelector('#editButton');
+  
     deleteButton.innerHTML = `
-        <i class="fa-solid fa-trash-can me-1"></i>
-        Delete
-    `;
-    cancelButton.classList.add('d-none'); 
+          <i class="fa-solid fa-trash-can me-1"></i>
+          Delete
+      `;
+    cancelButton.classList.add('d-none');
     deleteButton.classList.remove('btn-danger');
     deleteButton.classList.add('btn-outline-danger');
-    editButton.classList.remove('d-none'); 
+    editButton.classList.remove('d-none');
     deleteConfirmed = false;
-}
-
-function deleteItem() {
-    document.getElementById('deleteLink').click()
-}
+  }
+  
+  /**
+   * Toggles the delete confirmation in the user interface.
+   * @returns {void}
+   */
+  function toggleDeleteConfirmation() {
+    const cancelButton = document.querySelector('#cancelButton');
+    const deleteButton = document.getElementById('deleteButton');
+    const editButton = document.querySelector('#editButton');
+  
+    if (!deleteConfirmed) {
+      deleteButton.innerHTML = `
+              <i class="fa-solid fa-exclamation-circle me-1"></i>
+              Confirm Delete
+          `;
+      cancelButton.classList.remove('d-none');
+      deleteButton.classList.remove('btn-outline-danger');
+      deleteButton.classList.add('btn-danger');
+      editButton.classList.add('d-none');
+      deleteConfirmed = true;
+    } else {
+      deleteItem();
+      resetButtons();
+    }
+  }
--- testing.md
+++ testing.md
@@ -7,10 +7,22 @@ Back to [README.md](/README.md)
 ### HTML
 |Page|Validator|Result|
 | --- | --- | --- |
-| Home |![home]() | <mark>PASS<mark> |
-| All Items |![All items]() | <mark>PASS<mark> |
-| Item |![Item](./assets/testing/html-validator/single-listing.PNG) | <mark>PASS<mark> |
-| Create And Edit Item |![Create Listing]() | <mark>PASS<mark> |
-| Log In |![Log In]() | <mark>PASS<mark> |
-| Sign Up |![Sign Up]() | <mark>PASS<mark> |
-| Sign Out Conf |![home]() | <mark>PASS<mark> |
+| Home |![home](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validator-html-home.png) | <mark>PASS<mark> |
+| All Items |![All items](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validador-html-items.png) | <mark>PASS<mark> |
+| Item |![Item](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validador-html-item.png) | <mark>PASS<mark> |
+| Create And Edit Item |![Create Listing](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validator-html-add.png) | <mark>PASS<mark> |
+| Log In |![Log In](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validator-html-login.png) | <mark>PASS<mark> |
+| Sign Up |![Sign Up](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validator-html-signup.png) | <mark>PASS<mark> |
+
+### CSS
+
+▷ While the live site's running smooth without any noticeable glitches, I'm wrestling with Bootstrap 5 variables to make them play nice with the CSS validation engine. It's a bit of a challenge, but I'm diving in to crack the code
+
+Test Results CSS  <mark>FAIL<mark>
+
+### JavaScript
+
+- script.js <mark>PASS<mark> 
+
+[Validate JavaScript](https://github.com/mistersouza/localle/blob/main/static/assets/images/localle_validator-js.png)
+
